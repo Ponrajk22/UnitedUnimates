31ef08afa257ae7f463162befd89cc80979b6b80
ponrajkrish <ponrajkrish@172.16.127.1>
2017-11-19 05:52:10 +0800

    RB file

---
+++ Ec2/client.rb
@@ -0,0 +1,4 @@
+log_location     STDOUT
+chef_server_url  "https://api.chef.io/organizations/unitedunimates"
+validation_client_name "unitedunimates-validator"
+validation_key "/etc/chef/unitedunimates_validator.pem"
